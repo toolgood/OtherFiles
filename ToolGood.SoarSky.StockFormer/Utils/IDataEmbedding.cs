5f4c31e0eecf6c64b80b797706a36b09f0659202
toolgood <toolgood@qq.com>
2023-03-14 19:47:06 +0800

    修改

--- ToolGood.SoarSky.StockFormer/Utils/IDataEmbedding.cs
+++ ToolGood.SoarSky.StockFormer/Utils/IDataEmbedding.cs
@@ -39,14 +39,14 @@ namespace ToolGood.SoarSky.StockFormer.Utils
     public class DataEmbedding_wo_pos : nn.Module
     {
         public Dropout dropout;
-        public PositionalEmbedding position_embedding;
+       // public PositionalEmbedding position_embedding;
         public Module<Tensor, Tensor> temporal_embedding;
         public TokenEmbedding value_embedding;
 
         public DataEmbedding_wo_pos(int c_in, int d_model, string embed_type = "fixed", string freq = "h", double dropout = 0.1) : base("DataEmbedding_wo_pos")
         {
             this.value_embedding = new TokenEmbedding(c_in: c_in, d_model: d_model);
-            this.position_embedding = new PositionalEmbedding(d_model: d_model);
+          //  this.position_embedding = new PositionalEmbedding(d_model: d_model);
             this.temporal_embedding = embed_type != "timeF" ? new TemporalEmbedding(d_model: d_model, embed_type: embed_type, freq: freq) : new TimeFeatureEmbedding(d_model: d_model, embed_type: embed_type, freq: freq);
             this.dropout = nn.Dropout(p: dropout);
         }
